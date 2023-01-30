51bc83c5147cc37b580059ad2a6e31de5d4cd30e
Nadezhda Traykova <n.l.traykova@gmail.com>
2023-01-30 17:19:24 +0200

    Add project files.

---
+++ TestCollections/TestCollections.cs
@@ -0,0 +1,16 @@
+namespace TestCollections
+{
+    public class TestCollections
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
